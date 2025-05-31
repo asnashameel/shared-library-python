cc23b3446628f7222cdfeb4798f5be07e69964ea
asnashameel <2020.cse.asna@ekc.edu.in>
2025-05-31 15:02:05 +0000

    added

--- vars/build.groovy
+++ vars/build.groovy
@@ -2,7 +2,7 @@ def call() {
   sh '''
   python3 -m venv myenv
   . myenv/bin/activate
-  python3 pip install --upgrade pip
+  python3 -m pip install --upgrade pip
   pip install -r requirements.txt
   python3 -m pytest tests_main.py
   '''
