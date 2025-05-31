eb3fd484bb30091cf375e9e26cb62beca397d422
asnashameel <2020.cse.asna@ekc.edu.in>
2025-05-31 14:59:10 +0000

    changed

--- build.groovy
+++
@@ -1,8 +0,0 @@
-def call() {
-  sh '''
-  python3 -m venv myenv
-  . myenv/bin/activate
-  python3 -m pip install --upgrade pip
-  pip install -r requirements.txt
-  pthon3 -m pytest tests_main.py
-}
